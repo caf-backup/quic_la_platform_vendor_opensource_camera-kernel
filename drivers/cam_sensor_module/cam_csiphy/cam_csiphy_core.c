43c95e4af8675fd50549f34b12b3c015a1fd77b4
Petar Sivenov <quic_c_psiven@quicinc.com>
2022-01-19 10:46:42 +0200

    msm: camera: csiphy: fixes CSIPHY release error for combo mode
    The release of combo mode was broken because of incorrect CSIPHY
    release sequence. This change fixes the condition to bypass release in
    case of shared mode.
    
    Change-Id: I0fa3b63fbbf2d7682f95776e7fa6423e22139734
    Signed-off-by: Petar Sivenov <quic_c_psiven@quicinc.com>

--- drivers/cam_sensor_module/cam_csiphy/cam_csiphy_core.c
+++ drivers/cam_sensor_module/cam_csiphy/cam_csiphy_core.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -1067,7 +1067,7 @@ int32_t cam_csiphy_core_cfg(void *phy_dev,
 		}
 
 		csiphy_dev->share_count--;
-		if (csiphy_dev->combo_mode != 0 || csiphy_dev->share_count > 0) {
+		if (csiphy_dev->combo_mode == 0 && csiphy_dev->share_count > 0) {
 			CAM_DBG(CAM_CSIPHY, "Reuse device, skip release");
 			goto release_mutex;
 		}
